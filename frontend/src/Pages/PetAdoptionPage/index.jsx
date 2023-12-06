eb92779dd1ddb9795bf9b80850d7e5721901e200
Tahasp <tahasaeed221@gmail.com>
2023-12-05 23:48:15 -0500

    Added file 2

--- frontend/src/Pages/PetAdoptionPage/index.jsx
+++ frontend/src/Pages/PetAdoptionPage/index.jsx
@@ -6,8 +6,12 @@ import { Routes, Route , useNavigate, useParams } from "react-router-dom";
 
 const PetAdoptionPage = () => {
     const [pet_info, setPetInfo] = useState({});
-    
     let { petId } = useParams();
+
+    const [application_data, setApplicationData] = useState({
+      content: '',
+      listing: petId
+    });
 
     const get_pet_info = () => {
         fetch(`http://127.0.0.1:8000/listings/${petId}`, {
@@ -18,6 +22,44 @@ const PetAdoptionPage = () => {
             setPetInfo(data)
         })
     }
+
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    const handleChange = (event) => {
+        // const {id, value} = event.target
+        // const updatedApplicationData = {
+        //     content: application_data.content
+        // }
+        // updatedApplicationData[id] = value
+        // setApplicationData(updatedApplicationData)
+        setApplicationData({
+          ...application_data,
+          content: event.target.value,
+        });
+    }
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+
+      const response = await fetch("http://127.0.0.1:8000/applications", {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+              content: application_data.content,
+              listing: petId
+            })
+      })
+      try {
+        const response_value = await response
+        if (response_value.status === 200) {
+            navigate('/applications')
+        }
+      } catch {
+          console.log("register error occurred")
+      }
+  }
 
     useEffect(() => {
         get_pet_info()
@@ -32,32 +74,37 @@ const PetAdoptionPage = () => {
             <img src="./img/Mr%20Biscuit.jpg" />
           </div>
     
-          <div class="input-group">
-            <span class="input-group-text material-symbols-outlined">badge</span>
-            <input type="text" placeholder="Name" class="form-control" required />
+          <div className="input-group">
+            <span className="input-group-text material-symbols-outlined">badge</span>
+            <input type="text" placeholder="Name" className="form-control" required />
           </div>
     
-          <div class="input-group">
-            <span class="input-group-text material-symbols-outlined">email</span>
-            <input type="email" placeholder="Email" class="form-control" required />
+          <div className="input-group">
+            <span className="input-group-text material-symbols-outlined">email</span>
+            <input type="email" placeholder="Email" className="form-control" required />
           </div>
     
-          <div class="input-group">
-            <span class="input-group-text material-symbols-outlined">phone</span>
-            <input type="tel" placeholder="Phone Number" class="form-control" required />
+          <div className="input-group">
+            <span className="input-group-text material-symbols-outlined">phone</span>
+            <input type="tel" placeholder="Phone Number" className="form-control" required />
           </div>
     
-          <div class="input-group">
-            <span class="input-group-text material-symbols-outlined">reorder</span>
-            <textarea placeholder="Message to shelter (optional)" class="form-control" rows="4"></textarea>
+          <div className="input-group">
+            <span className="input-group-text material-symbols-outlined">reorder</span>
+            <textarea placeholder="Message to shelter (optional)" 
+              className="form-control" 
+              rows="4" 
+              onChange={handleChange}
+              value={application_data.content}
+            />
           </div>
     
-          <div class="agreement">
-            <input class="form-check-input" type="checkbox" id="agree" required />
-            <label class="form-check-label" for="agree">I agree to the <a href="">Adoption Terms of Service</a></label>
+          <div className="agreement">
+            <input className="form-check-input" type="checkbox" id="agree" required />
+            <label className="form-check-label" for="agree">I agree to the <a href="">Needs Fixing Adoption Terms of Service</a></label>
           </div>
     
-          <button type="submit" class="btn btn-primary d-flex">
+          <button type="submit" className="btn btn-primary d-flex" onClick={handleSubmit}>
             Apply
           </button>
         </form>
